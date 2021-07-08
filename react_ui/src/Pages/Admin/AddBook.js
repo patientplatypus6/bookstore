367aea4379aaccae1e38f2adcdb7333af14d46e8
Peter Weyand <patientplatypus6@gmail.com>
2021-07-08 19:28:27 -0400

    upload pic ui work

--- react_ui/src/Pages/Admin/AddBook.js
+++ react_ui/src/Pages/Admin/AddBook.js
@@ -13,9 +13,26 @@ const AddBook = () => {
   const rcnames = useSelector((state)=>state.revenuecost.rcnames)
   const indexvals = useSelector((state)=>state.revenuecost.indexvals)
 
+  const [image64List, setImage64List] = useState([])
+
   useEffect(()=>{
     console.log('value of rcnames: ', rcnames)
+    console.log('value of image64List: ', image64List)
   })
+
+  const imageDisplayHandler = () => {
+    return(
+      <div>
+        {image64List.map((image64, key)=>{
+          return(
+            <div key={key}>
+              <img src={image64} style={{height: 'auto', width: '10vw'}}/>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 
   const revenuecostHandler=()=>{
     console.log('inside revenuecostHandler  ')
@@ -37,107 +54,157 @@ const AddBook = () => {
 
   return(
     <>
-    <div
-      style={{
-        background: 'grey', 
-        display: 'inline-block', 
-        padding: '20px', 
-        verticalAlign: 'top', 
-        marginRight: '20px'
-      }}
-    >
       <div
         style={{
-          fontWeight: 'bold', 
-          fontSize: '1.5rem', 
-          display: 'inline-block'
+          background: 'grey', 
+          display: 'inline-block', 
+          padding: '20px', 
+          verticalAlign: 'top', 
+          marginRight: '20px'
         }}
       >
-        Upload Book List JSON
+        <div
+          style={{
+            fontWeight: 'bold', 
+            fontSize: '1.5rem', 
+            display: 'inline-block'
+          }}
+        >
+          Upload Book List JSON
+        </div>
+        <br/>
+        <br/>
+        <input type='file' multiple/>
       </div>
-      <br/>
-      <br/>
-      <input type='file' multiple/>
-    </div>
-    <div
-      style={{
-        background: 'grey', 
-        display: 'inline-block', 
-        padding: '20px', 
+      <div
+        style={{
+          background: 'rgb(100,0,0)', 
+          display: 'inline-block', 
+          padding: '20px', 
+          verticalAlign: 'top',
+        }}    
+      >
+        <div
+          style={{
+            background: 'grey', 
+            display: 'inline-block', 
+            padding: '20px', 
 
-      }}
-    >
-      <div
-        style={{
-          fontWeight: 'bold', 
-          fontSize: '1.5rem', 
-          display: 'inline-block'
-        }}
-      >
-        Add Book
+          }}
+        >
+          <div
+            style={{
+              fontWeight: 'bold', 
+              fontSize: '1.5rem', 
+              display: 'inline-block'
+            }}
+          >
+            Add Book
+          </div>
+          <br/>
+          <br/>
+          <div>
+            ISBN
+            <br/>
+            <InputBox title='isbn'/>
+          </div>
+          <br/>
+          <div>
+            Title
+            <br/>
+            <InputBox title='title'/>
+          </div>
+          <br/>
+          <div>
+            Sub Title
+            <br/>
+            <InputBox title='subtitle'/>
+          </div>
+          <br/>
+          <div>
+            Publisher
+            <br/>
+            <InputBox title='publisher'/>
+          </div>
+          <br/>
+          <div>
+            Current Copyright
+            <br/>
+            <InputBox title='currentcopyright'/>
+          </div>
+          <br/>
+          <div>
+            Edition
+            <br/>
+            <InputBox title='bookedition'/>
+          </div>
+          <br/>
+          <div>
+            Author Biography
+            <br/>
+            <InputBox title='authorbio'/>
+          </div>
+          <br/>
+          <div>
+            Synopsis
+            <br/>
+            <InputBox title='synopsis'/>
+          </div>
+          <br/>
+          {revenuecostHandler()}
+          <div>
+            <Button 
+              buttonName='addrevenuecost'
+              displayName="Add Revenue Cost"
+            />
+          </div>
+          <br/>
+          <div>
+            <Button buttonName='addbook' displayName="Add Book"/>
+          </div>
+        </div>
+        <div
+          style={{
+            background: 'grey', 
+            display: 'inline-block', 
+            padding: '20px', 
+            marginLeft: '5px',
+            verticalAlign: 'top',
+          }}
+        >
+          <div
+            style={{
+              fontWeight: 'bold', 
+              fontSize: '1.5rem', 
+              display: 'inline-block'
+            }}
+          >
+            Add Book Images
+          </div>
+          <br/><br/>
+          <input type='file' multiple
+            onChange={(e)=>{
+              const fileList = e.target.files
+              var base64List = []
+              const readerHandler = (i) => {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                  base64List.push(e.target.result)
+                  if(i<fileList.length - 1){
+                    i++
+                    readerHandler(i)
+                  }else if (i == fileList.length - 1){
+                    setImage64List(base64List)
+                  }
+                } 
+                reader.readAsDataURL(fileList[i])
+              }
+              readerHandler(0)
+            }}
+          />
+          {imageDisplayHandler()}
+        </div>
       </div>
-      <br/>
-      <br/>
-      <div>
-        ISBN
-        <br/>
-        <InputBox title='isbn'/>
-      </div>
-      <br/>
-      <div>
-        Title
-        <br/>
-        <InputBox title='title'/>
-      </div>
-      <br/>
-      <div>
-        Sub Title
-        <br/>
-        <InputBox title='subtitle'/>
-      </div>
-      <br/>
-      <div>
-        Publisher
-        <br/>
-        <InputBox title='publisher'/>
-      </div>
-      <br/>
-      <div>
-        Current Copyright
-        <br/>
-        <InputBox title='currentcopyright'/>
-      </div>
-      <br/>
-      <div>
-        Edition
-        <br/>
-        <InputBox title='bookedition'/>
-      </div>
-      <br/>
-      <div>
-        Author Biography
-        <br/>
-        <InputBox title='authorbio'/>
-      </div>
-      <br/>
-      <div>
-        Synopsis
-        <br/>
-        <InputBox title='synopsis'/>
-      </div>
-      <br/>
-      {revenuecostHandler()}
-      <div>
-        <Button 
-          buttonName='addrevenuecost'
-          displayName="Add Revenue Cost"
-        />
-      </div>
-      <br/>
-      <div>
-        <Button buttonName='addbook' displayName="Add Book"/>
-      </div>
-    </div>
     </>
   )
 }
